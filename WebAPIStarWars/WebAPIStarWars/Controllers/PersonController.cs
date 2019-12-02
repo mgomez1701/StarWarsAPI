836a311bdf78472adddadfd47ccd67ce8e57713d
Mike <michael005@me.com>
2019-12-01 20:23:18 -0500

    First Commit

---
+++ WebAPIStarWars/WebAPIStarWars/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIStarWars.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public PersonController(IHttpClientFactory client)
+        {
+            _client = client.CreateClient();
+            _client.BaseAddress = new Uri ("https://swapi.co/api/")
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAPIStarWars/WebAPIStarWars/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAPIStarWars.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
